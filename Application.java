829554ffb30eca334ff9134bccf77faae84d609a
Wingemo <philip.wingemo@outlook.com>
2022-12-06 17:01:16 +0100

    Update Application.java

--- Application.java
+++ Application.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.Properties;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            String configFilePath = "src/config.properties";
-            FileInputStream propsInput = new FileInputStream(configFilePath);
-            Properties prop = new Properties();
-            prop.load(propsInput);
+  public static void main(String[] args) {
+    try {
+      String configFilePath = "src/config.properties";
+      FileInputStream propsInput = new FileInputStream(configFilePath);
+      Properties prop = new Properties();
+      prop.load(propsInput);
 
-            Queue queue = new Queue(prop)
-            queue.execute()
-            queue.shutdown()
+      Queue queue = new Queue(prop)
+      queue.execute()
+      queue.shutdown()
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
 }
