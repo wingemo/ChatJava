45e2a39d5488a95a8f71c11b57c90e8c8040c1e1
Wingemo <philip.wingemo@outlook.com>
2022-12-06 17:00:55 +0100

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,16 +1,16 @@
 class Task {
-	private Integer first, second;
-	
-	Task(Integer first, Integer second) { 
-		this.first = first; 
-		this.second = second;
-	}
-	
-	Integer getFirst() {
-		return first;
-	}
-	
-	Integer getSecond() {
-		return second;
-	}
+  private Integer first, second;
+
+  Task(Integer first, Integer second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  Integer getFirst() {
+    return first;
+  }
+
+  Integer getSecond() {
+    return second;
+  }
 }
