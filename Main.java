a465a9b8365fc38bc25811bc08b2bcd2d10f987e
Wingemo <philip.wingemo@outlook.com>
2022-12-06 02:14:07 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Properties;
+
+public class DBHandlertest {
+
+    public static void main(String[] args) {
+        try {
+            String configFilePath = "src/config.properties";
+            FileInputStream propsInput = new FileInputStream(configFilePath);
+            Properties prop = new Properties();
+            prop.load(propsInput);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
