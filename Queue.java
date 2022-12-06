8488040f34a9fe53432fed1349a79d4f0987f4db
Wingemo <philip.wingemo@outlook.com>
2022-12-06 02:34:51 +0100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -3,17 +3,20 @@ import java.util.concurrent.BlockingQueue;
 
 public class Queue {
        BlockingQueue queue;
+       ExecutorService executor;
 
        public Main(int year, String name) {
               queue = new ArrayBlockingQueue(10);
+              executor = Executors.newFixedThreadPool(5);
         }
 
         static void myMethod() {
-              new Thread(new Producer(queue))).start();
-              (new Thread(new Producer(queue))).start();
-	      (new Thread(new Producer(queue))).start();
-	      (new Thread(new Consumer(queue))).start();
-	      (new Thread(new Consumer(queue))).start();
-	      (new Thread(new Consumer(queue))).start();        
+          for (int i = 0; i < 5; i++) {  
+            Runnable worker = new WorkerThread("" + i);  
+            executor.execute(worker); 
+            Runnable consumer = new WorkerThread("" + i);  
+            executor.execute(consumer); 
+          }  
+          executor.shutdown();       
 	}
 }
