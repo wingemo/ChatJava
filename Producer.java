393f422364c569e8e57c095409229a5f99b5c86a
Wingemo <philip.wingemo@outlook.com>
2022-12-06 17:02:55 +0100

    Update Producer.java

--- Producer.java
+++ Producer.java
@@ -2,24 +2,24 @@ import java.util.concurrent.BlockingQueue;
 import java.util.Random;
 
 public class Producer implements Runnable {
-    private static Random rand = new Random();
-    protected BlockingQueue queue = null;
+  private static Random rand = new Random();
+  protected BlockingQueue queue = null;
 
-    public Producer(BlockingQueue queue) {
-        this.queue = queue;
-    }
+  public Producer(BlockingQueue queue) {
+    this.queue = queue;
+  }
 
-    public void run() {
-        int one;
-        int two;
-        try {
-            while (true) {
-                one = rand.nextInt(10000);
-                two = rand.nextInt(10000);
-                queue.put(new Task(one, two));
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+  public void run() {
+    int one;
+    int two;
+    try {
+      while (true) {
+        one = rand.nextInt(10000);
+        two = rand.nextInt(10000);
+        queue.put(new Task(one, two));
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
