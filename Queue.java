021ad8e4b3b4d60d2ebf14ef0df65294328a0d6e
Wingemo <philip.wingemo@outlook.com>
2022-12-06 16:48:57 +0100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -3,20 +3,20 @@ import java.util.concurrent.BlockingQueue;
 
 public class Queue {
        BlockingQueue queue;
-       ExecutorService executor;
+       ExecutorService pool;
        Bank bank;
 
        public Queue(int year, String name) {
            queue = new ArrayBlockingQueue(10);
-           executor = Executors.newFixedThreadPool(5);
+           pool = Executors.newFixedThreadPool(5);
         }
 
         static void execute() {
           for (int i = 0; i < 5; i++) {  
             Runnable producer = new Producer(prop, bank, queue);  
-            executor.execute(producer)
+            pool.execute(producer)
             Runnable consumer = new Consumer(prop, bank, queue);  
-            executor.execute(consumer); 
+            pool.execute(consumer); 
           }        
 	}
 
