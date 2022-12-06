dbfc6e143dee6989bcc00c6009ef3aec53b7226d
Wingemo <philip.wingemo@outlook.com>
2022-12-06 02:53:01 +0100

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,16 @@
+class Task {
+	private Integer first, second;
+	
+	Task(Integer first, Integer second) { 
+		this.first = first; 
+		this.second = second;
+	}
+	
+	Integer getFirst() {
+		return first;
+	}
+	
+	Integer getSecond() {
+		return second;
+	}
+}
